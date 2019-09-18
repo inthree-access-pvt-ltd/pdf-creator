6e71d300b1f5fda01946fc7cd5c6d37f7c81b7d7
perumal <perumal@inthreeaccess.com>
2019-09-18 18:57:37 +0530

    changed

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello world!</h1><h2>PDF Created Successfully</h2><h4>Deva Added</h4>');
+$mpdf->WriteHTML('<h1>Hello world!</h1><h2>PDF Created Successfully</h2><h4>Deva Added</h4><h5>perumal updated</h5>');
 $mpdf->Output();
 ?>
