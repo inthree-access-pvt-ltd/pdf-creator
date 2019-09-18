df6f5712a6a41cb11ffbb9fe5d00988c00b96f12
devaraj <devaraj@inthreeaccess.com>
2019-09-18 18:49:47 +0530

    my first commit

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello world!</h1><h2>PDF Created Successfully</h2>');
+$mpdf->WriteHTML('<h1>Hello world!</h1><h2>PDF Created Successfully</h2><h4>Deva Added</h4>');
 $mpdf->Output();
 ?>
